614c92429b8e8955e679bb6ffe081dca9aff7e2d
Subbu <gandrala@gmail.com>
2015-02-26 23:06:27 +0800

    Organized imports

--- RestAPI/src/com/sfdc/local/SFDCPostData.java
+++ RestAPI/src/com/sfdc/local/SFDCPostData.java
@@ -4,14 +4,12 @@
 package com.sfdc.local;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicHeader;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
